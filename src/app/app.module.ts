b82562f96a290f09b01cca8dce57aa662985faa2
ibrahim <ibrahimljlidi23@gmail.com>
2024-02-21 17:22:19 +0100

    fix(COMPONANT): create-pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/admin/login/login.component';
+import { LayoutComponent } from './pages/admin/layout/layout.component';
+import { AirportComponent } from './pages/admin/airport/airport.component';
+import { CityComponent } from './pages/admin/city/city.component';
+import { BookingsComponent } from './pages/admin/bookings/bookings.component';
+import { NewFlightComponent } from './pages/admin/new-flight/new-flight.component';
+import { AllFlightsComponent } from './pages/admin/all-flights/all-flights.component';
+import { SearchComponent } from './pages/website/search/search.component';
+import { BookiFlightComponent } from './pages/website/booki-flight/booki-flight.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LayoutComponent,
+    AirportComponent,
+    CityComponent,
+    BookingsComponent,
+    NewFlightComponent,
+    AllFlightsComponent,
+    SearchComponent,
+    BookiFlightComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
